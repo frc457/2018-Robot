3b5a40cfc8cad9c0447686cecc4eeb55fa33da80
benji z <spcoxion@gmail.com>
2018-03-20 11:07:42 -0500

    Remove mandible control from triggers

--- src/main/java/org/greasemonkeys457/robot2018/commands/MandibleFromJoysticks.java
+++ src/main/java/org/greasemonkeys457/robot2018/commands/MandibleFromJoysticks.java
@@ -17,31 +17,12 @@ public class MandibleFromJoysticks extends Command {
         // Grab input from the controller
         double leftJoystick = -Robot.oi.operatorController.getRawAxis(1);
         double rightJoystick = -Robot.oi.operatorController.getRawAxis(5);
-        //double rightTrigger = Robot.oi.operatorController.getRawAxis(3);
-        //double leftTrigger = Robot.oi.operatorController.getRawAxis(2);
-        // TODO: Triggers are disabled here because they're being used for the elevator.
-        // Reimplement triggers once we have position control for the elevator
-        double leftTrigger = 0;
-        double rightTrigger = 0;
 
-        if (Robot.oi.operatorUp.get()) Robot.mandible.setSpeed(0.4);
-        else if (Robot.oi.operatorDown.get()) Robot.mandible.setSpeed(-0.4);
+        // Run the mandible using the D-pad if the D-pad is being used
+        if (Robot.oi.operatorUp.get()) Robot.mandible.setSpeed(1.0);
+        else if (Robot.oi.operatorDown.get()) Robot.mandible.setSpeed(-1.0);
 
-        // Decide what control method to use
-        else if (Math.abs(leftTrigger) > 0.01) {
-
-            // Run the motors inward at the same speed
-            Robot.mandible.setSpeed(-leftTrigger);
-
-        }
-
-        else if (Math.abs(rightTrigger) > 0.01) {
-
-            // Run the motors outward at the same speed
-            Robot.mandible.setSpeed(rightTrigger);
-
-        }
-
+        // Use the joysticks otherwise
         else {
 
             // Run the motors using the joysticks
